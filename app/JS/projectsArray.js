74f17e9af7303edd340a201536f04770e4cdd167
danilkberlin <danilkravets19@gmail.com>
2024-07-05 08:57:59 +0200

    Crerate js whis one array for portfolio

---
+++ app/JS/projectsArray.js
@@ -0,0 +1,21 @@
+const portfolios = [
+  {
+    id: 1,
+    src: "/assets/img/project3.jpeg",
+  },
+
+  {
+    id: 2,
+    src: "/assets/img/project2.jpeg",
+  },
+
+  {
+    id: 3,
+    src: "/assets/img/project1.jpeg",
+  },
+
+  {
+    id: 4,
+    src: "/assets/img/project4.jpeg",
+  },
+];
