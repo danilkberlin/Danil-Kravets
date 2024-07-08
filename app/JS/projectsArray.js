a6303108f033b6e6c588a530cc70c31d08990059
danilkberlin <danilkravets19@gmail.com>
2024-07-08 10:46:44 +0200

    I have built an Array for html code

--- app/JS/projectsArray.js
+++ app/JS/projectsArray.js
@@ -1,31 +1,50 @@
-const portfolios = [
+const imgArray = [
   {
     id: 1,
-    src: "/assets/img/project3.jpeg",
+    img: "/assets/img/project3.jpeg",
   },
-
   {
     id: 2,
-    src: "/assets/img/project2.jpeg",
+    img: "/assets/img/project4.jpeg",
   },
-
   {
     id: 3,
-    src: "/assets/img/project1.jpeg",
+    img: "/assets/img/project1.jpeg",
   },
-
   {
     id: 4,
-    src: "/assets/img/project4.jpeg",
+    img: "/assets/img/project2.jpeg",
   },
 ];
 
-const portfoliosCards = document.querySelector("card-portfolio");
+console.table(imgArray);
 
-portfolios.forEach((portfolio) => {
+for (var i = 0; i < imgArray.length; i++) {
+  //Div class Col-5
+  const col = document.createElement("div");
+  col.classList.add("col-lg-5");
+  col.classList.add("col-md-6");
+  col.classList.add("col-sm-12");
+  col.classList.add("my-3");
+
+  //Div class Card
   const card = document.createElement("div");
   card.classList.add("card");
-  card.innerHTML = `<img src="${portfolio.src}" alt="" />`;
-});
 
-console.log(portfoliosCards);
+  //Img tag
+
+  const img = document.createElement("img");
+  img.src = imgArray[i].img;
+  img.classList.add("card-img-top");
+  img.classList.add("img-fluid");
+  img.classList.add("rounded-2");
+  img.classList.add("projects-img-w");
+  console.log(img);
+
+  //all column + card + img
+  col.appendChild(card);
+  card.appendChild(img);
+
+  const appChildRow = document.querySelector("#cardArray>.row");
+  appChildRow.appendChild(col);
+}
