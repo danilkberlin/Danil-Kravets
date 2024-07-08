78d5170f2219fdd8264fa84d85dfdccb1384f682
danilkberlin <danilkravets19@gmail.com>
2024-07-08 17:20:56 +0200

    Create 2 pages whis js first about and next tools

---
+++ app/JS/about.js
@@ -0,0 +1,22 @@
+const MyData = [
+  {
+    title: "Name",
+    value: "Danylo Kravets",
+  },
+  {
+    title: "Phone",
+    value: "+4915225988431",
+  },
+  {
+    title: "Address",
+    value: "Germany, Berlin",
+  },
+  {
+    title: "Email",
+    value: "danilkravets19@gmail.com",
+  },
+];
+
+// Displaying the data
+const NewDate = new Date();
+const YearsOfExperience = NewDate.getFullYear() - 2022;
