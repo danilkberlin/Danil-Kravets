d56ae7ff8dfdc2c1ee789ce41e022ebc0c812d4e
danilkberlin <danilkravets19@gmail.com>
2024-07-08 17:26:16 +0200

    Whreit tools dates

---
+++ app/JS/tools.js
@@ -0,0 +1,50 @@
+const tools = [
+  {
+    icon: "fab fa-html5",
+    title: "HTML",
+  },
+  {
+    icon: "fab fa-css3-alt",
+    title: "CSS",
+  },
+  {
+    icon: "fab fa-js-square",
+    title: "JavaScript",
+  },
+  {
+    icon: "fab fa-react",
+    title: "React",
+  },
+  {
+    icon: "fab fa-node-js",
+    title: "Node.js",
+  },
+  {
+    icon: "fab fa-bootstrap",
+    title: "Bootstrap",
+  },
+  {
+    icon: "fab fa-tailwindcss",
+    title: "Tailwindcss",
+  },
+  {
+    icon: "fab fa-sass",
+    title: "Sass",
+  },
+  {
+    icon: "fab fa-adobe",
+    title: "Illustrator",
+  },
+  {
+    icon: "fab fa-figma",
+    title: "Figma",
+  },
+  {
+    icon: "fab fa-photo-album",
+    title: "Photoshop",
+  },
+  {
+    icon: "fab fa-vscode",
+    title: "VSCode",
+  },
+];
