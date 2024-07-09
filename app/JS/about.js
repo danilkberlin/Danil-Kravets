75be622a1a0647eb1e6c40e43087d6f9fb792b25
danilkberlin <danilkravets19@gmail.com>
2024-07-09 15:27:35 +0200

    Wrote the file to the specified location and return the resulting file name

--- app/JS/about.js
+++ app/JS/about.js
@@ -16,3 +16,37 @@ const MyData = [
     value: "danilkravets19@gmail.com",
   },
 ];
+
+const AboutCards = [
+  {
+    title: "Frontend development",
+    buttonStart: "UI/UX Desing",
+    buttonEnd: "Desing to Code",
+    comment: "I work with HTML/CSS, Framer and WardPress.",
+    imgSrc: "/assets/img/aboutImgCard1.jpeg",
+  },
+  {
+    title: "SEO/Digital marketing solutions",
+    buttonStart: "Social Media",
+    buttonEnd: "Analytics",
+    comment:
+      "Social media content plans, media monitoring, email and text messaging and search engine optimization.",
+    imgSrc: "/assets/img/aboutImgCard2.jpeg",
+  },
+  {
+    title: "Brand identity",
+    buttonStart: "Logo Desing",
+    buttonEnd: "Style Guldes",
+    comment:
+      "I help my clients to develop a personality and brand voice, design the brand look and logo.",
+    imgSrc: "/assets/img/aboutImgCard3.jpeg",
+  },
+  {
+    title: "Digital art and graphic design",
+    buttonStart: "Illustations",
+    buttonEnd: "  Al Experiments",
+    comment:
+      "I use Adobe Photoshop, Fresco and Blender to create illustrations and 3D renders for my clients and stocks.",
+    imgSrc: "/assets/img/aboutImgCard4.jpeg",
+  },
+];
