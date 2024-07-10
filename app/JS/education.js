4cb207af2701a84edde9e89bc27190c87f897eaf
danilkberlin <danilkravets19@gmail.com>
2024-07-10 12:39:07 +0200

    wrote a cycle that reproduces my education data

--- app/JS/education.js
+++ app/JS/education.js
@@ -43,7 +43,15 @@ const WorkExperience = [
     profession: "Practice at Turbine Kreuzberg GmbH",
     education: "Gained experience in front-end development during internship",
   },
+  {
+    year: "2024",
+    location: "Backend Developer",
+    profession: "Practice at Turbine Kreuzberg GmbH",
+    education: "Gained experience in back-end development during internship",
+  },
 ];
+
+console.table(WorkExperience);
 
 for (let i = 0; i < educations.length; i++) {
   //Creatre a new div for each items
@@ -104,4 +112,65 @@ for (let i = 0; i < educations.length; i++) {
   //Append the new row to the main section
   const addEducation = document.querySelector(".addEducation");
   addEducation.appendChild(newRow);
+}
+
+for (let i = 0; i < WorkExperience.length; i++) {
+  //Creat a new div for each items
+  const newRowForWork = document.createElement("div");
+  newRowForWork.classList.add(
+    "row",
+    "border-top",
+    "border-bottom",
+    "align-items-center",
+    "py-4"
+  );
+
+  //Create a new column 2
+  const newCol2ForWork = document.createElement("div");
+  newCol2ForWork.classList.add("col-2");
+
+  //Create a new column 5
+  const newCol5ForLocationForWork = document.createElement("div");
+  newCol5ForLocationForWork.classList.add("col-5");
+
+  //Create a new column 5
+  const newCol5ForEducationForWork = document.createElement("div");
+  newCol5ForEducationForWork.classList.add("col-5");
+
+  //Create a p tag for year
+  const newYearForWork = document.createElement("p");
+  newYearForWork.classList.add("mb-0");
+  newYearForWork.textContent = WorkExperience[i].year;
+
+  //Create a h3 tag for location
+  const newLocationForWork = document.createElement("h3");
+  newLocationForWork.classList.add("mb-0");
+  newLocationForWork.textContent = WorkExperience[i].location;
+
+  //Create a p tag for profession
+  const newProfessionForWork = document.createElement("p");
+  newProfessionForWork.classList.add("mb-0");
+  newProfessionForWork.textContent = WorkExperience[i].profession;
+
+  //Create a p tag for education
+  const newEducationForWork = document.createElement("p");
+  newEducationForWork.classList.add("mb-0");
+  newEducationForWork.textContent = WorkExperience[i].education;
+
+  //Create a col2 > p tag for year
+  newCol2ForWork.appendChild(newYearForWork);
+  //Create a col5ForLocation > h3 tag for location and p tag for profession
+  newCol5ForLocationForWork.appendChild(newLocationForWork);
+  newCol5ForLocationForWork.appendChild(newProfessionForWork);
+  //Create a col5ForEducation > p tag for education
+  newCol5ForEducationForWork.appendChild(newEducationForWork);
+
+  //Append the new columns to the new row
+  newRowForWork.appendChild(newCol2ForWork);
+  newRowForWork.appendChild(newCol5ForLocationForWork);
+  newRowForWork.appendChild(newCol5ForEducationForWork);
+
+  //Append the new row to the main section
+  const addWorkExperience = document.querySelector(".addWorkExperience");
+  addWorkExperience.appendChild(newRowForWork);
 }
