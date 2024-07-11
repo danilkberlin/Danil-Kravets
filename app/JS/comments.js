4490b31823b3591035cc6bf2f56ece5988923b27
danilkberlin <danilkravets19@gmail.com>
2024-07-11 16:40:42 +0200

    Create a comments

--- app/JS/comments.js
+++ app/JS/comments.js
@@ -81,3 +81,35 @@ const comments = [
     button: `<button class="rounded-4 px-4 py-1">Project Page <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path></svg></button>`,
   },
 ];
+
+for (let i = 0; i < comments.length; i++) {
+  let comment = comments[i];
+
+  // Create a THML format
+
+  let CarouselHtml = `<div class="carousel-item">
+        <div class="row my-3  align-items-center justify-content-start">
+            <div class='col-md-2 col-4'>
+                <img src="${comment.img}" class="rounded-circle img-fluid">
+            </div>
+            <div class='col-md-10 col-8'>
+                <h3 class='text-color'>${comment.name}</h3>
+                <p class='text-color m-0'>${comment.text}</p>
+            </div>
+        </div>
+        <div class="row my-3 justify-content-start">
+            <div class="col">
+                <p class='text-color'>${comment.comment}</p>
+            </div>
+        </div>
+        <div class="row my-3 justify-content-start">
+            <div class="col">
+                ${comment.button}
+            </div>
+        </div>
+    </div>
+    `;
+
+  // Append the HTML to the carousel
+  document.querySelector(".carouserComments").innerHTML += CarouselHtml;
+}
