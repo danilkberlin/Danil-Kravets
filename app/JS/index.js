cd2aef80763c14a5a876f0cf6e087c62e0eb1bd9
danilkberlin <danilkravets19@gmail.com>
2024-07-13 02:39:07 +0200

    update data

--- app/JS/index.js
+++ app/JS/index.js
@@ -98,3 +98,203 @@ function updateCardFixed() {
 
 updateCardFixed();
 window.addEventListener("resize", updateCardFixed);
+
+for (let i = 0; i < comments.length; i++) {
+  let comment = comments[i];
+
+  // Create a THML format
+
+  let CarouselHtml = `<div class="carousel-item">
+        <div class="row my-3  align-items-center justify-content-start">
+            <div class='col-md-2 col-4'>
+                <img src="${comment.img}" class="rounded-circle img-fluid">
+            </div>
+            <div class='col-md-10 col-8'>
+                <h3 class='text-color'>${comment.name}</h3>
+                <p class='text-color m-0'>${comment.text}</p>
+            </div>
+        </div>
+        <div class="row my-3 justify-content-start">
+            <div class="col">
+                <p class='text-color'>${comment.comment}</p>
+            </div>
+        </div>
+        <div class="row my-3 justify-content-start">
+            <div class="col">
+                ${comment.button}
+            </div>
+        </div>
+    </div>
+    `;
+
+  // Append the HTML to the carousel
+  document.querySelector(".carouserComments").innerHTML += CarouselHtml;
+}
+
+for (let i = 0; i < educations.length; i++) {
+  //Creatre a new div for each items
+  const newRow = document.createElement("div");
+  newRow.classList.add(
+    "row",
+    "border-top",
+    "border-bottom",
+    "align-items-center",
+    "py-4"
+  );
+
+  //Create a new column 2
+  const newCol2 = document.createElement("div");
+  newCol2.classList.add("col-2");
+
+  //Create a new column 5
+  const newCol5ForLocation = document.createElement("div");
+  newCol5ForLocation.classList.add("col-5");
+
+  //Create a new column 5
+  const newCol5ForEducation = document.createElement("div");
+  newCol5ForEducation.classList.add("col-5");
+
+  //Create a p tag for year
+  const newYear = document.createElement("p");
+  newYear.classList.add("mb-0", "text-grei");
+  newYear.textContent = educations[i].year;
+
+  //Create a h3 tag for location
+  const newLocation = document.createElement("h3");
+  newLocation.classList.add("mb-0", "text-color");
+  newLocation.textContent = educations[i].location;
+
+  //Create a span tag for span
+  const newSpan = document.createElement("span");
+  newSpan.classList.add("text-color");
+  newSpan.textContent = educations[i].span;
+
+  //Create a p tag for profession
+  const newProfession = document.createElement("p");
+  newProfession.classList.add("mb-0", "text-grei");
+  newProfession.textContent = educations[i].profession;
+
+  //Create a p tag for education
+  const newEducation = document.createElement("p");
+  newEducation.classList.add("mb-0", "text-grei");
+  newEducation.textContent = educations[i].education;
+
+  //Create a col2 > p tag for year
+  newCol2.appendChild(newYear);
+  //Create a col5ForLocation > h3 tag for location and p tag for profession
+  newProfession.appendChild(newSpan);
+  newCol5ForLocation.appendChild(newLocation);
+  newCol5ForLocation.appendChild(newProfession);
+  //Create a col5ForEducation > p tag for education
+  newCol5ForEducation.appendChild(newEducation);
+
+  //Append the new columns to the new row
+  newRow.appendChild(newCol2);
+  newRow.appendChild(newCol5ForLocation);
+  newRow.appendChild(newCol5ForEducation);
+
+  //Append the new row to the main section
+  const addEducation = document.querySelector(".addEducation");
+  addEducation.appendChild(newRow);
+}
+
+for (let i = 0; i < WorkExperience.length; i++) {
+  //Creat a new div for each items
+  const newRowForWork = document.createElement("div");
+  newRowForWork.classList.add(
+    "row",
+    "border-top",
+    "border-bottom",
+    "align-items-center",
+    "py-4"
+  );
+
+  //Create a new column 2
+  const newCol2ForWork = document.createElement("div");
+  newCol2ForWork.classList.add("col-2");
+
+  //Create a new column 5
+  const newCol5ForLocationForWork = document.createElement("div");
+  newCol5ForLocationForWork.classList.add("col-5");
+
+  //Create a new column 5
+  const newCol5ForEducationForWork = document.createElement("div");
+  newCol5ForEducationForWork.classList.add("col-5");
+
+  //Create a p tag for year
+  const newYearForWork = document.createElement("p");
+  newYearForWork.classList.add("mb-0", "text-grei");
+  newYearForWork.textContent = WorkExperience[i].year;
+
+  //Create a h3 tag for location
+  const newLocationForWork = document.createElement("h3");
+  newLocationForWork.classList.add("mb-0", "text-color");
+  newLocationForWork.textContent = WorkExperience[i].location;
+
+  //Create a p tag for profession
+  const newProfessionForWork = document.createElement("p");
+  newProfessionForWork.classList.add("mb-0", "text-grei");
+  newProfessionForWork.textContent = WorkExperience[i].profession;
+
+  //Create a span tag for span
+  const newSpanForWork = document.createElement("span");
+  newSpanForWork.classList.add("text-color");
+  newSpanForWork.textContent = WorkExperience[i].span;
+
+  //Create a p tag for education
+  const newEducationForWork = document.createElement("p");
+  newEducationForWork.classList.add("mb-0", "text-grei");
+  newEducationForWork.textContent = WorkExperience[i].education;
+
+  //Create a col2 > p tag for year
+  newCol2ForWork.appendChild(newYearForWork);
+  //Create a col5ForLocation > h3 tag for location and p tag for profession
+
+  newCol5ForLocationForWork.appendChild(newLocationForWork);
+  newCol5ForLocationForWork.appendChild(newProfessionForWork);
+  newProfessionForWork.appendChild(newSpanForWork);
+  //Create a col5ForEducation > p tag for education
+  newCol5ForEducationForWork.appendChild(newEducationForWork);
+
+  //Append the new columns to the new row
+  newRowForWork.appendChild(newCol2ForWork);
+  newRowForWork.appendChild(newCol5ForLocationForWork);
+  newRowForWork.appendChild(newCol5ForEducationForWork);
+
+  //Append the new row to the main section
+  const addWorkExperience = document.querySelector(".addWorkExperience");
+  addWorkExperience.appendChild(newRowForWork);
+}
+
+for (var i = 0; i < tools.length; i++) {
+  // Create a new column
+  const newCol = document.createElement("div");
+  newCol.classList.add("mb-2", "col-md-2", "col-4");
+
+  // Create a new card
+  const card = document.createElement("div");
+  card.classList.add("nav-button-content", "rounded-4", "text-center");
+
+  //Create a new img tag for icon
+  const img = document.createElement("img");
+  img.src = tools[i].icon;
+  img.classList.add("img-fluid", "w-50", "h-50", "my-2");
+
+  // Create a new h6 tag for title
+  const title = document.createElement("h6");
+  title.classList.add("text-color", "mb-3");
+  title.textContent = tools[i].title;
+
+  // Append the new elements to the card
+  card.appendChild(img);
+  card.appendChild(title);
+
+  // Append the new card to the new column
+  newCol.appendChild(card);
+
+  // Append the new column to the new row
+
+  // Append the new row to the main section
+  const MyFavoriteTools = document.querySelector(".MyFavoriteTools>.row");
+  MyFavoriteTools.appendChild(newCol);
+}
--- app/JS/tools.js
+++ app/JS/tools.js
@@ -48,38 +48,3 @@ const tools = [
     title: "VSCode",
   },
 ];
-
-console.table(tools);
-
-for (var i = 0; i < tools.length; i++) {
-  // Create a new column
-  const newCol = document.createElement("div");
-  newCol.classList.add("mb-2", "col-md-2", "col-4");
-
-  // Create a new card
-  const card = document.createElement("div");
-  card.classList.add("nav-button-content", "rounded-4", "text-center");
-
-  //Create a new img tag for icon
-  const img = document.createElement("img");
-  img.src = tools[i].icon;
-  img.classList.add("img-fluid", "w-50", "h-50", "my-2");
-
-  // Create a new h6 tag for title
-  const title = document.createElement("h6");
-  title.classList.add("text-color", "mb-3");
-  title.textContent = tools[i].title;
-
-  // Append the new elements to the card
-  card.appendChild(img);
-  card.appendChild(title);
-
-  // Append the new card to the new column
-  newCol.appendChild(card);
-
-  // Append the new column to the new row
-
-  // Append the new row to the main section
-  const MyFavoriteTools = document.querySelector(".MyFavoriteTools>.row");
-  MyFavoriteTools.appendChild(newCol);
-}
